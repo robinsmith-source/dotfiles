6685b0157db21d6ab00626cb7731160492ad78a5
Robin Schmidt <schmidtrobin02@gmail.com>
2026-05-06 07:47:27 +0200

    feat: add keybinds to navigate between multiple outputs,

--- .config/nvim/lua/matugen.lua
+++ .config/nvim/lua/matugen.lua
@@ -3,23 +3,23 @@
  function M.setup()
    require('base16-colorscheme').setup {
      -- Background tones
-     base00 = '#121416', -- Default Background
-     base01 = '#1e2022', -- Lighter Background (status bars)
-     base02 = '#282a2d', -- Selection Background
-     base03 = '#8c9198', -- Comments, Invisibles
+     base00 = '#121316', -- Default Background
+     base01 = '#1f1f23', -- Lighter Background (status bars)
+     base02 = '#292a2d', -- Selection Background
+     base03 = '#8e9099', -- Comments, Invisibles
      -- Foreground tones
-     base04 = '#c2c7cf', -- Dark Foreground (status bars)
-     base05 = '#e2e2e5', -- Default Foreground
-     base06 = '#e2e2e5', -- Light Foreground
-     base07 = '#e2e2e5', -- Lightest Foreground
+     base04 = '#c4c6d0', -- Dark Foreground (status bars)
+     base05 = '#e3e2e6', -- Default Foreground
+     base06 = '#e3e2e6', -- Light Foreground
+     base07 = '#e3e2e6', -- Lightest Foreground
      -- Accent colors
      base08 = '#ffb4ab', -- Variables, XML Tags, Errors
-     base09 = '#d3bfe6', -- Integers, Constants
-     base0A = '#b9c8da', -- Classes, Search Background
-     base0B = '#97cbff', -- Strings, Diff Inserted
-     base0C = '#d3bfe6', -- Regex, Escape Chars
-     base0D = '#97cbff', -- Functions, Methods
-     base0E = '#b9c8da', -- Keywords, Storage
+     base09 = '#debcdf', -- Integers, Constants
+     base0A = '#bec6dc', -- Classes, Search Background
+     base0B = '#acc7ff', -- Strings, Diff Inserted
+     base0C = '#debcdf', -- Regex, Escape Chars
+     base0D = '#acc7ff', -- Functions, Methods
+     base0E = '#bec6dc', -- Keywords, Storage
      base0F = '#93000a', -- Deprecated, Embedded Tags
    }
  end
